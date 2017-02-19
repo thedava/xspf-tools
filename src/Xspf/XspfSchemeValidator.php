a16c6e7293c18305465d9482fff38bd9df4df75d
thedava <tehdava@googlemail.com>
2017-02-19 15:39:58 +0100

    Added detection for invalid validation result

--- src/Xspf/XspfSchemeValidator.php
+++ src/Xspf/XspfSchemeValidator.php
@@ -9,12 +9,19 @@ class XspfSchemeValidator
     const RESULT_SUCCESS = '<span class="valid">Valid</span>';
     const RESULT_ERROR = '<span class="invalid">Invalid</span>';
 
+    /**
+     * Validate the given xml using the xspf online validator
+     *
+     * @param string $xml
+     *
+     * @return bool|null
+     */
     public static function isValid($xml)
     {
         $data = [
-            'pasted' => $xml,
-            'uploaded' => '',
-            'url' => '',
+            'pasted'       => $xml,
+            'uploaded'     => '',
+            'url'          => '',
             'submitPasted' => 'Submit',
         ];
 
@@ -26,8 +33,25 @@ class XspfSchemeValidator
             ],
         ];
         $context = stream_context_create($options);
-        $result = file_get_contents(self::URL, false, $context);
 
-        return (strpos($result, self::RESULT_SUCCESS) !== false && strpos($result, self::RESULT_ERROR) === false);
+        try {
+            $result = file_get_contents(self::URL, false, $context);
+            if (!is_string($result)) {
+                return null;
+            }
+
+            $isSuccess = strpos($result, self::RESULT_SUCCESS) !== false;
+            $isFailure = strpos($result, self::RESULT_ERROR) !== false;
+
+            // Unexpected validation result
+            if (!$isSuccess && !$isFailure) {
+                return null;
+            }
+
+            return ($isSuccess && !$isFailure);
+
+        } catch (\Exception $error) {
+            return null;
+        }
     }
 }
