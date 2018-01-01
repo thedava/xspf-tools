7d3f73bc35d825c6fa477ec1b7757134ca5b83d2
thedava <thedava@users.noreply.github.com>
2018-01-01 17:41:35 +0100

    Updated phplint.sh according to shellcheck

--- bin/phplint.sh
+++ bin/phplint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-find ./bin -name "*.php" -type f | xargs -n 1 php -l
-find ./src -name "*.php" -type f | xargs -n 1 php -l
-find ./test -name "*.php" -type f | xargs -n 1 php -l
+find ./bin -name "*.php" -type f -print0 | xargs -n 1 -0 php -l
+find ./src -name "*.php" -type f -print0 | xargs -n 1 -0 php -l
+find ./test -name "*.php" -type f -print0 | xargs -n 1 -0 php -l
