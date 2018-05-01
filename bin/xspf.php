7bd20bb70a04e914b225d3f39f37ca5ee0c8e54c
thedava <thedava@users.noreply.github.com>
2018-05-01 14:57:15 +0200

    Added fallback fnmatch function

--- bin/xspf.php
+++ bin/xspf.php
@@ -11,6 +11,34 @@ set_error_handler(function ($errno, $errstr, $errfile, $errline, array $errconte
     throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
 });
 
+if (!function_exists('fnmatch')) {
+    defined('FNM_NOESCAPE') || define ('FNM_NOESCAPE', 1);
+    defined('FNM_PATHNAME') || define ('FNM_PATHNAME', 2);
+    defined('FNM_PERIOD') || define ('FNM_PERIOD', 4);
+    defined('FNM_CASEFOLD') || define ('FNM_CASEFOLD', 16);
+
+    /**
+     * Match filename against a pattern
+     *
+     * @link http://php.net/manual/en/function.fnmatch.php
+     *
+     * @param string $pattern
+     * @param string $string
+     * @param int $flags [optional]
+     *
+     * @return bool true if there is a match, false otherwise.
+     */
+    function fnmatch($pattern, $string, $flags = 0)
+    {
+        if ($flags & FNM_CASEFOLD) {
+            $pattern = mb_strtolower($pattern);
+            $string = mb_strtolower($string);
+        }
+
+        return preg_match('#^' . strtr(preg_quote($pattern, '#'), ['\*' => '.*', '\?' => '.']) . '$#i', $string);
+    }
+}
+
 try {
     $application = new \Symfony\Component\Console\Application('XSPF Tools', Utils::getVersion());
 
