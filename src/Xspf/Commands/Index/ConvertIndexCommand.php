d4f23e2e2abced600a43129402092d3653311bab
thedava <thedava@users.noreply.github.com>
2018-11-18 01:05:15 +0100

    Added ordering to index:convert

--- src/Xspf/Commands/Index/ConvertIndexCommand.php
+++ src/Xspf/Commands/Index/ConvertIndexCommand.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Xspf\Commands\CreateCommand;
 use Xspf\Index\IndexModel;
+use Xspf\Order\AbstractOrderType;
 use Xspf\Track;
 
 class ConvertIndexCommand extends CreateCommand
@@ -19,7 +20,8 @@ class ConvertIndexCommand extends CreateCommand
             ->addArgument('index-file', InputArgument::OPTIONAL, 'The index file', 'index.' . IndexModel::EXT_COMPRESSED)
             ->addArgument('playlist-file', InputArgument::OPTIONAL, 'The playlist file that should be created', 'index.xspf')
             ->addOption('distinct', 't', InputOption::VALUE_NONE, 'Avoid duplicates in playlist file')
-            ->addOption('delete', 'D', InputOption::VALUE_NONE, 'Remove index file after conversion');
+            ->addOption('delete', 'D', InputOption::VALUE_NONE, 'Remove index file after conversion')
+            ->addOption('order', '', InputOption::VALUE_REQUIRED, 'Order the index file (asc, desc, random)', null);
         $this->appendWhiteAndBlacklistOptions($this);
     }
 
@@ -28,6 +30,12 @@ class ConvertIndexCommand extends CreateCommand
         $this->parseWhiteAndBlacklist($input);
         $indexModel = new IndexModel($input->getArgument('index-file'));
         $indexModel->load();
+
+        $order = $input->getOption('order');
+        if ($order !== null) {
+            $orderType = AbstractOrderType::factory($order);
+            $orderType->orderIndex($indexModel);
+        }
 
         $skipCount = 0;
         $history = new \ArrayObject();
