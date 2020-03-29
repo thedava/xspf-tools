f74487cb50787fad39fb0ba4ccfe0fc4bd2d5bd7
thedava <thedava@users.noreply.github.com>
2020-03-29 23:02:43 +0200

    Implemented Xspf\Console\Application wrapper & builder class

--- console.php
+++ console.php
@@ -1,47 +1,28 @@
 <?php
 
-use Symfony\Component\Console\Formatter\OutputFormatterStyle;
-use Symfony\Component\Console\Output\ConsoleOutput;
-use Xspf\Utils;
-
 ini_set('display_errors', 'On');
 error_reporting(-1);
 
 $applicationTitle = 'XSPF Tools';
 if (PHP_MAJOR_VERSION < 7) {
     echo $applicationTitle, PHP_EOL,
-        PHP_EOL,
-        'Minimum required version is 7.0 but ', phpversion(), ' was given',
-        PHP_EOL;
+    PHP_EOL,
+    'Minimum required version is 7.0 but ', phpversion(), ' was given',
+    PHP_EOL;
     exit(1);
 }
 
 require_once __DIR__ . '/vendor/autoload.php';
-Utils::setDirectory(__DIR__);
+Xspf\Utils::setDirectory(__DIR__);
 
 set_error_handler(function ($errno, $errstr, $errfile, $errline) {
     throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
 });
 
 try {
-    // Styling and coloring
-    $output = new ConsoleOutput();
-    $formatter = $output->getFormatter();
-    $formatter->setStyle('cyan', new OutputFormatterStyle('cyan'));
-    $formatter->setStyle('green', new OutputFormatterStyle('green'));
-    $formatter->setStyle('red', new OutputFormatterStyle('red'));
-    $formatter->setStyle('yellow', new OutputFormatterStyle('yellow'));
-    $formatter->setStyle('blue', new OutputFormatterStyle('blue'));
+    $application = new Xspf\Console\Application();
 
-    $application = new Symfony\Component\Console\Application($applicationTitle, Utils::getVersion());
-
-    // Append commands
-    foreach (require_once __DIR__ . '/data/console-commands.php' as $command) {
-        $application->add(new $command());
-    }
-
-    $application->run(null, $output);
-    exit(0);
+    exit($application->run());
 } catch (Exception $error) {
     echo 'An unexpected error occured!', PHP_EOL, PHP_EOL;
     echo $error->getMessage(), PHP_EOL;
---
+++ src/Xspf/Console/Application.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Xspf\Console;
+
+use Symfony\Component\Console\Application as ConsoleApplication;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\OutputInterface;
+use Xspf\Utils;
+
+class Application
+{
+    /** @var ConsoleApplication */
+    protected $consoleApplication;
+
+    /**
+     * @return string
+     */
+    protected function getApplicationTitle(): string
+    {
+        return 'XSPF Tools';
+    }
+
+    /**
+     * @param ConsoleApplication $application
+     */
+    protected function appendCommands(ConsoleApplication $application)
+    {
+        // Append commands
+        foreach (require_once __DIR__ . '/../../../data/console-commands.php' as $command) {
+            $application->add(new $command());
+        }
+    }
+
+    /**
+     * @return ConsoleApplication
+     */
+    public function create(): ConsoleApplication
+    {
+        if ($this->consoleApplication === null) {
+            $this->consoleApplication = new ConsoleApplication($this->getApplicationTitle(), Utils::getVersion());
+            $this->appendCommands($this->consoleApplication);
+        }
+
+        return $this->consoleApplication;
+    }
+
+    /**
+     * @return OutputInterface
+     */
+    protected function getOutput(): OutputInterface
+    {
+        // Styling and coloring
+        $output = new ConsoleOutput();
+
+        $formatter = $output->getFormatter();
+        $formatter->setStyle('cyan', new OutputFormatterStyle('cyan'));
+        $formatter->setStyle('green', new OutputFormatterStyle('green'));
+        $formatter->setStyle('red', new OutputFormatterStyle('red'));
+        $formatter->setStyle('yellow', new OutputFormatterStyle('yellow'));
+        $formatter->setStyle('blue', new OutputFormatterStyle('blue'));
+
+        return $output;
+    }
+
+    /**
+     * @return int
+     * @throws \Exception
+     */
+    public function run(): int
+    {
+        return (int)$this->create()->run(null, $this->getOutput());
+    }
+}
