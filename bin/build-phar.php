3729c6458b399827da6eb8227d0e2ef044884bcd
thedava <thedava@users.noreply.github.com>
2021-01-05 02:39:38 +0100

    Replaced simple buildFromDirectory with more complex buildFromIterator in build-phar.php (allows much more filtering of e.g. unnecessary files in vendor dir)

--- bin/build-phar.php
+++ bin/build-phar.php
@@ -18,9 +18,41 @@ if (file_exists($pharFile)) {
     unlink($pharFile);
 }
 
+$baseDir = dirname(__DIR__);
+$baseDirOffset = strlen($baseDir) + 1;
+$iterator = new RecursiveIteratorIterator(
+    new RecursiveCallbackFilterIterator(
+        new RecursiveDirectoryIterator($baseDir),
+        function (SplFileInfo $file) use ($baseDirOffset) {
+            // BLACKLIST: by filename
+            $fileName = $file->getFilename();
+            if (in_array($fileName, ['', '.', '..', 'composer.lock', 'phpunit.xml', 'phpunit.xml.dist'])) {
+                return false;
+            }
+
+            // WHITELIST: by top level path (project root)
+            $relativePath = substr($file->getPathname(), $baseDirOffset);
+            if (preg_match('/^(data|src|composer\.json|console.php|VERSION)/', $relativePath) === 1) {
+                return true;
+            }
+
+            // BLACKLIST: by top level path, by any file pattern
+            if (
+                preg_match('/^(bin|test|build|\..*|Jenkinsfile|LICENSE|Tests|php-|php_|phpunit)/', $fileName) === 1
+                || preg_match('/\.(phar|md|json|lock)$/i', $fileName) === 1
+            ) {
+                return false;
+            }
+
+            return true;
+        }
+    )
+
+);
+
 $phar = new Phar($pharFile);
 $phar->setMetadata(['version' => $version]);
-$phar->buildFromDirectory(dirname(__DIR__), '/(VERSION|src|vendor|console\.php|data|composer\.json)/');
+$phar->buildFromIterator($iterator, $baseDir);
 $phar->setStub($phar->createDefaultStub('console.php'));
 $phar->compressFiles(Phar::GZ);
 
